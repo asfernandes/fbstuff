0233323e25aa60f0e52b85c1b4e9248ca9f071cf
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2011-05-15 20:15:28 -0300

    Test properties changed by the attachment charset.

--- src/test/v3api/DescribeTest.cpp
+++ src/test/v3api/DescribeTest.cpp
@@ -32,141 +32,152 @@ BOOST_AUTO_TEST_SUITE(v3api)
 
 BOOST_AUTO_TEST_CASE(describe)
 {
-	const string location = FbTest::getLocation("describe.fdb");
+	const unsigned char* testDpb[2] = {NULL, FbTest::ASCII_DPB};
+	unsigned testDpbLength[2] = {0, sizeof(FbTest::ASCII_DPB)};
+	unsigned testSubType[2] = {3, 2};
+	unsigned testLength[2] = {31 * 3, 31};
 
-	IStatus* status = master->getStatus();
-
-	IAttachment* attachment = dispatcher->createDatabase(status, location.c_str(), 0, NULL);
-	BOOST_CHECK(status->isSuccess());
-	BOOST_REQUIRE(attachment);
-
-	ITransaction* transaction = attachment->startTransaction(status, 0, NULL);
-	BOOST_CHECK(status->isSuccess());
-	BOOST_REQUIRE(transaction);
-
-	unsigned major;
-	getEngineVersion(attachment, &major);
-
-	// Test with and without metadata prefetch.
-	for (unsigned i = 0; i < 2; ++i)
+	for (unsigned test = 0; test < 2; ++test)
 	{
-		bool prefetch = (i == 0);
+		const string location = FbTest::getLocation("describe.fdb");
 
-		IStatement* stmt = attachment->allocateStatement(status);
+		IStatus* status = master->getStatus();
+
+		IAttachment* attachment = dispatcher->createDatabase(status, location.c_str(),
+			testDpbLength[test], testDpb[test]);
 		BOOST_CHECK(status->isSuccess());
-		BOOST_REQUIRE(stmt);
+		BOOST_REQUIRE(attachment);
 
-		stmt->prepare(status, transaction, 0,
-			"select rdb$relation_id relid, rdb$character_set_name csname"
-			"  from rdb$database"
-			"  where rdb$relation_id < ?",
-			FbTest::DIALECT, (prefetch ? IStatement::PREPARE_PREFETCH_ALL : 0));
+		ITransaction* transaction = attachment->startTransaction(status, 0, NULL);
 		BOOST_CHECK(status->isSuccess());
+		BOOST_REQUIRE(transaction);
 
-		unsigned type = stmt->getType(status);
-		BOOST_CHECK(status->isSuccess());
+		unsigned major;
+		getEngineVersion(attachment, &major);
 
-		string legacyPlan = stmt->getPlan(status, false);
-		BOOST_CHECK(status->isSuccess());
-
-		string detailedPlan = major >= 3 ? stmt->getPlan(status, true) : "";
-		BOOST_CHECK(status->isSuccess());
-
-		const IParametersMetadata* inputParams = stmt->getInputParameters(status);
-		BOOST_CHECK(status->isSuccess());
-		BOOST_REQUIRE(inputParams);
-
-		const IParametersMetadata* outputParams = stmt->getOutputParameters(status);
-		BOOST_CHECK(status->isSuccess());
-		BOOST_REQUIRE(outputParams);
-
-		BOOST_CHECK_EQUAL(type, isc_info_sql_stmt_select);
-
-		BOOST_CHECK_EQUAL(legacyPlan,
-			"\nPLAN (RDB$DATABASE NATURAL)");
-
-		if (major >= 3)
+		// Test with and without metadata prefetch.
+		for (unsigned i = 0; i < 2; ++i)
 		{
-			BOOST_CHECK_EQUAL(detailedPlan,
-				"\nSelect Expression\n"
-				"    -> Filter\n"
-				"        -> Table \"RDB$DATABASE\" Full Scan");
+			bool prefetch = (i == 0);
+
+			IStatement* stmt = attachment->allocateStatement(status);
+			BOOST_CHECK(status->isSuccess());
+			BOOST_REQUIRE(stmt);
+
+			stmt->prepare(status, transaction, 0,
+				"select rdb$relation_id relid, rdb$character_set_name csname"
+				"  from rdb$database"
+				"  where rdb$relation_id < ?",
+				FbTest::DIALECT, (prefetch ? IStatement::PREPARE_PREFETCH_ALL : 0));
+			BOOST_CHECK(status->isSuccess());
+
+			unsigned type = stmt->getType(status);
+			BOOST_CHECK(status->isSuccess());
+
+			string legacyPlan = stmt->getPlan(status, false);
+			BOOST_CHECK(status->isSuccess());
+
+			string detailedPlan = major >= 3 ? stmt->getPlan(status, true) : "";
+			BOOST_CHECK(status->isSuccess());
+
+			const IParametersMetadata* inputParams = stmt->getInputParameters(status);
+			BOOST_CHECK(status->isSuccess());
+			BOOST_REQUIRE(inputParams);
+
+			const IParametersMetadata* outputParams = stmt->getOutputParameters(status);
+			BOOST_CHECK(status->isSuccess());
+			BOOST_REQUIRE(outputParams);
+
+			BOOST_CHECK_EQUAL(type, isc_info_sql_stmt_select);
+
+			BOOST_CHECK_EQUAL(legacyPlan,
+				"\nPLAN (RDB$DATABASE NATURAL)");
+
+			if (major >= 3)
+			{
+				BOOST_CHECK_EQUAL(detailedPlan,
+					"\nSelect Expression\n"
+					"    -> Filter\n"
+					"        -> Table \"RDB$DATABASE\" Full Scan");
+			}
+
+			struct FieldInfo
+			{
+				string field;
+				string relation;
+				string owner;
+				string alias;
+				unsigned type;
+				bool nullable;
+				unsigned subType;
+				unsigned length;
+				unsigned scale;
+
+				static void test(IStatus* status, const IParametersMetadata* params,
+					unsigned count, FieldInfo* fieldInfo)
+				{
+					BOOST_CHECK_EQUAL(params->getCount(status), count);
+					BOOST_CHECK(status->isSuccess());
+
+					for (unsigned i = 0; i < count; ++i)
+					{
+						BOOST_CHECK_EQUAL(params->getField(status, i), fieldInfo[i].field);
+						BOOST_CHECK(status->isSuccess());
+
+						BOOST_CHECK_EQUAL(params->getRelation(status, i), fieldInfo[i].relation);
+						BOOST_CHECK(status->isSuccess());
+
+						BOOST_CHECK_EQUAL(params->getOwner(status, i), fieldInfo[i].owner);
+						BOOST_CHECK(status->isSuccess());
+
+						BOOST_CHECK_EQUAL(params->getAlias(status, i), fieldInfo[i].alias);
+						BOOST_CHECK(status->isSuccess());
+
+						BOOST_CHECK_EQUAL(params->getType(status, i), fieldInfo[i].type);
+						BOOST_CHECK(status->isSuccess());
+
+						BOOST_CHECK_EQUAL(params->isNullable(status, i), fieldInfo[i].nullable);
+						BOOST_CHECK(status->isSuccess());
+
+						BOOST_CHECK_EQUAL(params->getSubType(status, i), fieldInfo[i].subType);
+						BOOST_CHECK(status->isSuccess());
+
+						BOOST_CHECK_EQUAL(params->getLength(status, i), fieldInfo[i].length);
+						BOOST_CHECK(status->isSuccess());
+
+						BOOST_CHECK_EQUAL(params->getScale(status, i), fieldInfo[i].scale);
+						BOOST_CHECK(status->isSuccess());
+					}
+				}
+			};
+
+			FieldInfo inputInfo[] = {
+				{"", "", "", "", SQL_SHORT, true, 0, sizeof(SSHORT), 0}
+			};
+
+			FieldInfo::test(status, inputParams, sizeof(inputInfo) / sizeof(inputInfo[0]), inputInfo);
+
+			FieldInfo outputInfo[] = {
+				{"RDB$RELATION_ID",        "RDB$DATABASE", "SYSDBA", "RELID",  SQL_SHORT, true,
+					0,                 sizeof(SSHORT),   0},
+				{"RDB$CHARACTER_SET_NAME", "RDB$DATABASE", "SYSDBA", "CSNAME", SQL_TEXT,  true,
+					testSubType[test], testLength[test], 0}
+			};
+
+			FieldInfo::test(status, outputParams, sizeof(outputInfo) / sizeof(outputInfo[0]), outputInfo);
+
+			stmt->free(status, DSQL_drop);
+			BOOST_CHECK(status->isSuccess());
 		}
 
-		struct FieldInfo
-		{
-			string field;
-			string relation;
-			string owner;
-			string alias;
-			unsigned type;
-			bool nullable;
-			unsigned subType;
-			unsigned length;
-			unsigned scale;
-
-			static void test(IStatus* status, const IParametersMetadata* params,
-				unsigned count, FieldInfo* fieldInfo)
-			{
-				BOOST_CHECK_EQUAL(params->getCount(status), count);
-				BOOST_CHECK(status->isSuccess());
-
-				for (unsigned i = 0; i < count; ++i)
-				{
-					BOOST_CHECK_EQUAL(params->getField(status, i), fieldInfo[i].field);
-					BOOST_CHECK(status->isSuccess());
-
-					BOOST_CHECK_EQUAL(params->getRelation(status, i), fieldInfo[i].relation);
-					BOOST_CHECK(status->isSuccess());
-
-					BOOST_CHECK_EQUAL(params->getOwner(status, i), fieldInfo[i].owner);
-					BOOST_CHECK(status->isSuccess());
-
-					BOOST_CHECK_EQUAL(params->getAlias(status, i), fieldInfo[i].alias);
-					BOOST_CHECK(status->isSuccess());
-
-					BOOST_CHECK_EQUAL(params->getType(status, i), fieldInfo[i].type);
-					BOOST_CHECK(status->isSuccess());
-
-					BOOST_CHECK_EQUAL(params->isNullable(status, i), fieldInfo[i].nullable);
-					BOOST_CHECK(status->isSuccess());
-
-					BOOST_CHECK_EQUAL(params->getSubType(status, i), fieldInfo[i].subType);
-					BOOST_CHECK(status->isSuccess());
-
-					BOOST_CHECK_EQUAL(params->getLength(status, i), fieldInfo[i].length);
-					BOOST_CHECK(status->isSuccess());
-
-					BOOST_CHECK_EQUAL(params->getScale(status, i), fieldInfo[i].scale);
-					BOOST_CHECK(status->isSuccess());
-				}
-			}
-		};
-
-		FieldInfo inputInfo[] = {
-			{"", "", "", "", SQL_SHORT, true, 0, sizeof(SSHORT), 0}
-		};
-
-		FieldInfo::test(status, inputParams, sizeof(inputInfo) / sizeof(inputInfo[0]), inputInfo);
-
-		FieldInfo outputInfo[] = {
-			{"RDB$RELATION_ID",        "RDB$DATABASE", "SYSDBA", "RELID",  SQL_SHORT, true, 0, sizeof(SSHORT), 0},
-			{"RDB$CHARACTER_SET_NAME", "RDB$DATABASE", "SYSDBA", "CSNAME", SQL_TEXT,  true, 3, 31 * 3,         0}
-		};
-
-		FieldInfo::test(status, outputParams, sizeof(outputInfo) / sizeof(outputInfo[0]), outputInfo);
-
-		stmt->free(status, DSQL_drop);
+		transaction->commit(status);
 		BOOST_CHECK(status->isSuccess());
+
+		attachment->drop(status);
+		BOOST_CHECK(status->isSuccess());
+
+		status->dispose();
 	}
-
-	transaction->commit(status);
-	BOOST_CHECK(status->isSuccess());
-
-	attachment->drop(status);
-	BOOST_CHECK(status->isSuccess());
-
-	status->dispose();
 }
 
 
