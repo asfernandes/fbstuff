e27987062d0f90ea658aa7a4ba4166e1dccaa59f
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2015-03-11 12:26:04 -0300

    Change the license.

--- src/pascal/FbApiTest.dpr
+++ src/pascal/FbApiTest.dpr
@@ -1,22 +1,19 @@
 {
- *  The contents of this file are subject to the Initial
- *  Developer's Public License Version 1.0 (the "License");
- *  you may not use this file except in compliance with the
- *  License. You may obtain a copy of the License at
- *  http://www.ibphoenix.com/main.nfs?a=ibphoenix&page=ibp_idpl.
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
  *
- *  Software distributed under the License is distributed AS IS,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied.
- *  See the License for the specific language governing rights
- *  and limitations under the License.
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  The Original Code was created by Adriano dos Santos Fernandes.
+ * The Initial Developer of the Original Code is Adriano dos Santos Fernandes.
+ * Portions created by the Initial Developer are Copyright (C) 2015 the Initial Developer.
+ * All Rights Reserved.
  *
- *  Copyright (c) 2015 Adriano dos Santos Fernandes <adrianosf at gmail.com>
- *  and all contributors signed below.
- *
- *  All Rights Reserved.
- *  Contributor(s): ______________________________________.
+ * Contributor(s):
 }
 
 program FbApiTest;
